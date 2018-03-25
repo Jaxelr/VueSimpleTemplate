23857fac3397e9e50c2c7650e0504738aec34c0f
Jaxel Rojas <jaxelr@gmail.com>
2018-03-24 23:00:44 -0400

    Thought i removed these?

--- Content/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace VueTemplate.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index() => View();
-    }
-}
