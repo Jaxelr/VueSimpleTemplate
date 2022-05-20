e4503d8810027a62f7248a24e93785324c4842ce
Jaxel Rojas <jaxelr@gmail.com>
2022-05-20 08:44:57 -0400

    feat: refactor client app to target vue 3.0

--- Content/ClientApp/src/main.js
+++ Content/ClientApp/src/main.js
@@ -1,12 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
